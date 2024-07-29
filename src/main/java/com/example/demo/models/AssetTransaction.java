e7b4a2aac54c4a78a0cc8e4db51a67cc2f110aa2
Yoga Budiman <65494727+ygabdn29@users.noreply.github.com>
2024-07-29 09:42:38 +0700

    refactor: change data type for dates

--- src/main/java/com/example/demo/models/AssetTransaction.java
+++ src/main/java/com/example/demo/models/AssetTransaction.java
@@ -1,19 +1,21 @@
 package com.example.demo.models;
 
+import java.time.LocalDateTime;
+
 public class AssetTransaction {
   private Integer id;
-  private String requestSubmissionTime;
-  private String outTime;
-  private String returnTime;
+  private LocalDateTime requestSubmissionTime;
+  private LocalDateTime outTime;
+  private LocalDateTime returnTime;
   private Boolean isApprovedManager;
   private Boolean isApprovedAdmin;
   private Integer userId;
   private Integer adminId;
   private Integer assetId;
-  
 
-  public AssetTransaction(Integer id, String requestSubmissionTime, String outTime, String returnTime,
-      Boolean isApprovedManager, Boolean isApprovedAdmin, Integer userId, Integer adminId, Integer assetId) {
+  public AssetTransaction(Integer id, LocalDateTime requestSubmissionTime, LocalDateTime outTime,
+      LocalDateTime returnTime, Boolean isApprovedManager, Boolean isApprovedAdmin, Integer userId, Integer adminId,
+      Integer assetId) {
     this.id = id;
     this.requestSubmissionTime = requestSubmissionTime;
     this.outTime = outTime;
@@ -24,96 +26,81 @@ public class AssetTransaction {
     this.adminId = adminId;
     this.assetId = assetId;
   }
-
 
   public Integer getId() {
     return id;
   }
 
-
   public void setId(Integer id) {
     this.id = id;
   }
 
-
-  public String getRequestSubmissionTime() {
+  public LocalDateTime getRequestSubmissionTime() {
     return requestSubmissionTime;
   }
 
-
-  public void setRequestSubmissionTime(String requestSubmissionTime) {
+  public void setRequestSubmissionTime(LocalDateTime requestSubmissionTime) {
     this.requestSubmissionTime = requestSubmissionTime;
   }
 
-
-  public String getOutTime() {
+  public LocalDateTime getOutTime() {
     return outTime;
   }
 
-
-  public void setOutTime(String outTime) {
+  public void setOutTime(LocalDateTime outTime) {
     this.outTime = outTime;
   }
 
-
-  public String getReturnTime() {
+  public LocalDateTime getReturnTime() {
     return returnTime;
   }
 
-
-  public void setReturnTime(String returnTime) {
+  public void setReturnTime(LocalDateTime returnTime) {
     this.returnTime = returnTime;
   }
-
 
   public Boolean getIsApprovedManager() {
     return isApprovedManager;
   }
 
-
   public void setIsApprovedManager(Boolean isApprovedManager) {
     this.isApprovedManager = isApprovedManager;
   }
-
 
   public Boolean getIsApprovedAdmin() {
     return isApprovedAdmin;
   }
 
-
   public void setIsApprovedAdmin(Boolean isApprovedAdmin) {
     this.isApprovedAdmin = isApprovedAdmin;
   }
-
 
   public Integer getUserId() {
     return userId;
   }
 
-
   public void setUserId(Integer userId) {
     this.userId = userId;
   }
-
 
   public Integer getAdminId() {
     return adminId;
   }
 
-
   public void setAdminId(Integer adminId) {
     this.adminId = adminId;
   }
-
 
   public Integer getAssetId() {
     return assetId;
   }
 
-
   public void setAssetId(Integer assetId) {
     this.assetId = assetId;
   }
+  
+  
+ 
 
   
   
